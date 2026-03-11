a0af2bee2c5ff202a9203028cc1cecf1efd6ed75
Mar_Hiro <Ma.Hiro.Yogi.Carvalho@gmail.com>
2026-03-11 10:30:18 -0300

    Refactor data capture to include disk I/O rates

--- aplicacao-vooh/script_coleta_dados_hardware/coleta.py
+++ aplicacao-vooh/script_coleta_dados_hardware/coleta.py
@@ -4,58 +4,55 @@ import os;
 import csv;
 from datetime import date, datetime
 
+INTERVALO = 3
+
+
 def captura_dados():
-        
+    final_io = psutil.disk_io_counters()
     pular = "\n" * 2
-    # CAPTURA DE DADOS DA CPU
     while True: 
-        porcentagem_uso_da_cpu = psutil.cpu_percent(interval=None)
+        porcentagem_uso_da_cpu = psutil.cpu_percent(INTERVALO)
 
-        # CAPTURA DE DADOS DO DISCO
         porcentagem_uso_do_disco = psutil.disk_usage("/").percent 
-
-        # CAPTURA DE DADOS DA MEMÓRIA
+        initial_io = psutil.disk_io_counters()
+        
+        read_bytes_diff = initial_io.read_bytes - final_io.read_bytes 
+        write_bytes_diff = initial_io.write_bytes - final_io.write_bytes
+        
+        read_rate_Bps = read_bytes_diff / INTERVALO
+        write_rate_Bps = write_bytes_diff / INTERVALO
+        
         memoria = psutil.virtual_memory();  
         memoria_total = memoria.total;  
         memoria_disponivel = memoria.available
         memoria_percentual = (memoria_disponivel / memoria_total) * 100
 
         timestamp_atual = datetime.now()
-        # timestamp_formatado = timestamp_atual.strftime("%d-%m-%Y %H:%M:%S")
-        timestamp_formatado = timestamp_atual.isoformat()
+        timestamp_formatado = timestamp_atual.isoformat() # formato iso: YYYY-MM-DDThh:mm:ss
 
-        # TRANSFORMANDO EM DICIONÁRIO COM OS  DADOS
         conteudo_csv = {
             "usuario": os.getlogin(),
             "porcentagem_uso_da_cpu": porcentagem_uso_da_cpu,
             "porcentagem_uso_do_disco": porcentagem_uso_do_disco,
             "memoria_percentual": memoria_percentual,
-            "data": timestamp_formatado
+            "read_rate_Bps": read_rate_Bps,
+            "write_rate_Bps": write_rate_Bps,
+            "data": timestamp_formatado,
         }
-
-        # print(conteudo_csv)
-        # MANDANDO OS DADOS PARA INSERIR NO ARQUIVO .CSV
         inserir_arquivo_csv(conteudo_csv)    
-        # INTERVALO DE
-        # TEMPO PARA CAPTURA
-        time.sleep(3)    
-        # captura_dados()
+        time.sleep(INTERVALO)    
+        final_io = psutil.disk_io_counters()
 
 def inserir_arquivo_csv(dados):
-
     print(dados)
-
-    # PEGANDO A DATA PARA COLOCAR NO ARQUIVO
     nome_arquivo = f"coleta_{date.today()}.csv"
-
     with open(nome_arquivo, mode='a', newline='') as arquivo_csv:
         writer = csv.DictWriter(arquivo_csv, fieldnames=dados.keys(), delimiter=';')
-
-        # VALIDAÇÃO COM TELL() QUE É UMA FUNÇÃO QUE VERIFICA SE ESTÁ VAZIO OU NÃO
         if arquivo_csv.tell() == 0:
             writer.writeheader()
-
-        # ONDE ESTOU ESCREVENDO OS DADOS NO ARQUIVO
         writer.writerow(dados)
 
+
+
+#começar captura
 captura_dados()
