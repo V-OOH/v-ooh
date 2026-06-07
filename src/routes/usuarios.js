1b6d355f23d7786b16ef34f94fe3cabfc27192ba
MarleyS439 <marleysantos439@gmail.com>
2026-06-06 22:04:22 -0300

    Adição de rota para upload e busca de foto de perfil do usuário

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,34 +1,42 @@
 var express = require("express");
 var router = express.Router();
+const upload = require("../../config/uploadConfig");
 
 var usuarioController = require("../controllers/usuarioController");
 
 router.post("/cadastrar", function (req, res) {
-    usuarioController.cadastrar(req, res);
-})
+  usuarioController.cadastrar(req, res);
+});
 router.post("/autenticar", function (req, res) {
-    usuarioController.autenticar(req, res);
+  usuarioController.autenticar(req, res);
 });
 
 router.patch("/finalizarCadastro", function (req, res) {
-    usuarioController.finalizarCadastro(req, res);
+  usuarioController.finalizarCadastro(req, res);
 });
 
 router.post("/cadastrarFuncionario", function (req, res) {
-    usuarioController.cadastrarFuncionario(req, res);
+  usuarioController.cadastrarFuncionario(req, res);
 });
 
 router.post("/cadastrarGestor", function (req, res) {
-    usuarioController.cadastrarGestor(req, res);
+  usuarioController.cadastrarGestor(req, res);
 });
 
 router.post("/buscarUsuarios", function (req, res) {
-    usuarioController.buscarUsuarios(req, res);
+  usuarioController.buscarUsuarios(req, res);
 });
 
 router.post("/buscarNumUsuarios", function (req, res) {
-    usuarioController.buscarNumUsuarios(req, res);
+  usuarioController.buscarNumUsuarios(req, res);
 });
 
+router.post("/update-foto", upload.single("fotoPerfil"), (req, res) => {
+  usuarioController.updateFotoPerfil(req, res);
+});
+
+router.post("/foto-perfil", (req, res) => {
+  usuarioController.bucarFotoPerfil(req, res);
+});
 
 module.exports = router;
