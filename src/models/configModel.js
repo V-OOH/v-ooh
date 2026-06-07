15620c67b80bc600d2dc6f850f210e74e913fa4d
MarleyS439 <marleysantos439@gmail.com>
2026-06-06 22:03:58 -0300

    Ajustes de buscar dados

--- src/models/configModel.js
+++ src/models/configModel.js
@@ -1,55 +1,72 @@
-var database = require("../database/config")
+var database = require("../database/config");
 
 function buscarinfo(idUsuario) {
-    var instrucaoSql = `
-            SELECT *, DATE_FORMAT(data_hora_atualizacao, '%Y-%m-%dT%H:%i:%s') as dataAtualizacao FROM usuario WHERE id = ${idUsuario};
+  var instrucaoSql = `
+            SELECT *, DATE_FORMAT(dataAtualizacao , '%Y-%m-%dT%H:%i:%s') as dataAtualizacao FROM usuario WHERE usuario.idUsuario = ${idUsuario};
         `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-    
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function buscarempresa(fkEmpresa) {
-    var instrucaoSql = `
-            SELECT nome_empresa,cnpj FROM cadastroEmpresa WHERE id = ${fkEmpresa};
-        `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-    
+  var instrucaoSql = `
+    SELECT
+    nomeEmpresa,
+    CONCAT(
+            SUBSTRING(cnpj, 1, 2), '.',
+            SUBSTRING(cnpj, 3, 3), '.',
+            SUBSTRING(cnpj, 6, 3), '/',
+            SUBSTRING(cnpj, 9, 4), '-',
+            SUBSTRING(cnpj, 13, 2)
+    )  AS cnpj
+    FROM cadastroEmpresa WHERE cadastroEmpresa.idcadastroEmpresa = ${fkEmpresa};
+    `;
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function buscarendereco(fkEmpresa) {
-    var instrucaoSql = `
-            SELECT 
-            e.id,
-            e.cep,
-            e.logradouro,
-            e.numero,
-            e.bairro,
-            e.cidade,
-            e.uf,
-            e.complemento
-        FROM endereco e
-        INNER JOIN empresa emp ON emp.fk_endereco = e.id
-        WHERE emp.id = ${fkEmpresa};
-        `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-    
+  var sql = `
+    SELECT
+	CONCAT(
+	SUBSTRING(e.cep, 1, 5),
+	"-",
+	SUBSTRING(e.cep, 6, 8)
+	) AS cep,
+	e.logradouro,
+	e.numero,
+	IFNULL(e.complemento, "N/A") AS complemento,
+	e.bairro,
+	e.cidade,
+	e.uf
+    FROM endereco e JOIN cadastroEmpresa ce
+    ON e.fkEmpresa = ce.idcadastroEmpresa AND e.fkDisplay IS NULL;`;
+
+  console.log("Executando a instrução SQL: \n" + sql);
+  return database.executar(sql);
 }
 
-function editarendereco(fkEmpresa, idEndereco, cep, logradouro, numero, complemento, bairro, cidade, uf) {
-    var instrucaoSql = `     
-         UPDATE endereco SET cep = '${cep}',logradouro = '${logradouro}',numero = ${numero},complemento = '${complemento}',bairro = '${bairro}',cidade = '${cidade}', uf = '${uf}' WHERE idEndereco = ${idEndereco} and fkEmpresa =' ${fkEmpresa}'; 
+function editarendereco(
+  fkEmpresa,
+  idEndereco,
+  cep,
+  logradouro,
+  numero,
+  complemento,
+  bairro,
+  cidade,
+  uf,
+) {
+  var instrucaoSql = `
+         UPDATE endereco SET cep = '${cep}',logradouro = '${logradouro}',numero = ${numero},complemento = '${complemento}',bairro = '${bairro}',cidade = '${cidade}', uf = '${uf}' WHERE idEndereco = ${idEndereco} and fkEmpresa =' ${fkEmpresa}';
         `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-    
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 module.exports = {
-    buscarinfo,
-    buscarempresa,
-    buscarendereco,
-    editarendereco
+  buscarinfo,
+  buscarempresa,
+  buscarendereco,
+  editarendereco,
 };
